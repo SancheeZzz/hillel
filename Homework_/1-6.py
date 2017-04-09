c9108e6cd89749e786639167a8c49c96b5f12c6a
SancheeZzz <valera.redis@gmail.com>
2017-04-09 14:38:40 +0300

    Исправил ошибки в формулах

--- Homework_/1-6.py
+++ Homework_/1-6.py
@@ -4,13 +4,13 @@ b=25
 c=42
 result_1= a + b * (c/2)
 print("Answer of 1 task is -","%.2f"%result_1)
-result_2 = ((a*2)+(b*2))%2
+result_2 = ((a**2)+(b**2))%2
 print("Answer of 2 task is -","%.2f"%result_2)
 result_3=  (a+b)/ 12 * c % 4 + b
 print("Answer of 3 task is -","%.2f"%result_3)
 result_4= (a - b * c ) / ( a + b ) % c
 print("Answer of 4 task is -","%.2f"%result_4)
-result_5=  abs(a - b)  /( a + b)*3 - math.cos( c )
+result_5=  abs(a - b)  /( a + b)**3 - math.cos( c )
 print("Answer of 5 task is -","%.2f"%result_5)
-result_6= (math.log(1 + c)/(-b))*4 + abs(a)
+result_6= (math.log(1 + c)/(-b))**4 + abs(a)
 print("Answer of 6 task is -","%.2f"%result_6)
