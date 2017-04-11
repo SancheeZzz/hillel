cac52cec44bea42e3ec7abc9eba9f05181328b59
SancheeZzz <valera.redis@gmail.com>
2017-04-11 18:38:05 +0300

    Добавил симметричность. Убрал str() (ставил его, потому что если в формулу result подставить space_1, space_2 то оно выполняется с ошибкой. Я нашёл выход из положения просто вписав пробел вручную.) (почему то, найденный спэйс_1 и спэйс_2 позиционировались, как цифры 3 и 7)

--- Homework_/Task_9.py
+++ Homework_/Task_9.py
@@ -1,9 +1,9 @@
 s = "qwe rty uio"
-print("Start str. is =", s)
+print("Str. before upper is =", s)
 spase_1 = s.find(" ")
-space2 = s.find (" ", spase_1+1)
-mid_of_s = s[ spase_1 +1:space2]
-letters_1 = s[:spase_1]
-letters_2 = s[space2 + 1:]
-result = str(letters_1) + " " + str(mid_of_s.upper()) + " " + str(letters_2)
+space_2 = s.find (" ", spase_1+1)
+mid_of_s = s[ spase_1 +1:space_2]
+left_of_s = s[:spase_1]
+right_of_s = s[space_2 + 1:]
+result = left_of_s + " " + mid_of_s.upper() + " " + right_of_s
 print("Str. after upper=", result)
