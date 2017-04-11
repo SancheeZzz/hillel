1e0436ed4e7407bbb094f91df2d1834a16d85d43
SancheeZzz <valera.redis@gmail.com>
2017-04-11 20:04:59 +0300

    TEST_COMMITE

--- Homework_/Task_10.py
+++ Homework_/Task_10.py
@@ -4,7 +4,7 @@ idxstar_2 = s.find ("*",idxstar_1+1)
 name = s [:idxstar_1]
 idxdelimiter_1= s.find ("-")
 idxdelimiter_2 = s.find ("-", idxstar_2+1)
-date_of_birth = s [idxstar_1+1: idxdelimiter_1]
-date_of_death = s [idxstar_2+1: idxdelimiter_2]
-years_of_life = int(date_of_death) - int(date_of_birth)
+year_of_birth = s [idxstar_1+1: idxdelimiter_1]
+year_of_death = s [idxstar_2+1: idxdelimiter_2]
+years_of_life = int(year_of_death) - int(year_of_birth)
 print('"',name,'"',",",years_of_life)
