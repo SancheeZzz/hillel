7aa2cd26dc3785596541cab0dacc00312c8fd1b9
SancheeZzz <valera.redis@gmail.com>
2017-04-11 20:53:13 +0300

    Task_12

--- Homework_/Task_12.py
+++ Homework_/Task_12.py
@@ -1,13 +1,23 @@
+#1попытка
 # def sum_of_3_numbers (number):
 #     f_number = number.find(0)
 #     s_number = number.find(1)
 #     th_number = number.find(2)
 #     result = int(f_number)+ int(s_number) + int(th_number)
 #     return result
-#
-number = input("Enter random number " )
-f_number = number[0]
-s_number = number[0:1]
-th_number = number[1:2]
-sum_of_numbers = int(f_number)+ int(s_number) + int(th_number)
-print(sum_of_numbers)
+#2попытка
+#  f_number = number[0]
+# s_number = number[0:1]
+# th_number = number[1:2]
+# sum_of_numbers = int(f_number)+ int(s_number) + int(th_number)
+#3 попытка
+
+import math
+n = input("Enter random number ")
+n = int(n)
+
+f_number = n // 100
+s_number = n % 100 // 10
+th_number = n % 10
+
+print ("Sum of 3 number is -", f_number + s_number + th_number)
