8a68a3444efc661dd388da92d8c0bb46c91add53
SancheeZzz <valera.redis@gmail.com>
2017-04-13 11:16:35 +0300

    Task_13 (точно_Fixed)

--- Homework_/Task_13.py
+++ Homework_/Task_13.py
@@ -2,7 +2,14 @@ def square_of_right_triangle (catheter_1,catheter_2):
         result = 1/2 * (catheter_1*catheter_2)
         return result
 
+def perimeter_of_right_triangle(catheter_1,catheter_2):
+        import math
+        result= math.sqrt(catheter_1**2+catheter_2**2)
+        return result
+
 catheter_1 = int(input("Enter the length of the first catheter:" ))
 catheter_2 = int(input("Enter the length of the second catheter:" ))
 
-print(int(square_of_right_triangle(catheter_1,catheter_2)))
+print( "A square of right triangle -" ,int(square_of_right_triangle(catheter_1,catheter_2)))
+print( "A perimeter of right triangle -" ,int(perimeter_of_right_triangle(catheter_1,catheter_2)))
+# c**2=a**2 + b**2
