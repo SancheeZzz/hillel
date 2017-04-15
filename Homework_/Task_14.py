fcce1435e4d4bd67de15bfb27c398cd5b9193e22
SancheeZzz <valera.redis@gmail.com>
2017-04-15 14:47:28 +0300

    Task_14 Программа работает, но почему-то выдаёт вдобавок "None". Что я сделал не так ?

---
+++ Homework_/Task_14.py
@@ -0,0 +1,10 @@
+def parity (n):
+   if (n % 2 == 0):
+        print("The number is even")
+   else:
+        print ("The number isn`t even")
+   return
+n= int(input("Enter a random number: "))
+print (parity(n))
+
+
