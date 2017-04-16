70c8c263199b804dc461844e6bb011bad83f9cd6
SancheeZzz <valera.redis@gmail.com>
2017-04-17 01:47:09 +0300

    Task_17(как-то так... сделал без учёта того, что может быть либо 1 корень, либо корней вообще не может быть...я пока что не понимаю,  ведь перед нами стоит задача решить квадр. уравнение)

--- Homework_/Task_17.py
+++ Homework_/Task_17.py
@@ -1,21 +1,14 @@
-import math
-def quadratic_equation (a,b,D):
-    if D > 0:
-        D = math.sqrt(int(D))
-        x1 = (-b + D) / (2 * a)
-        print("x1 = ", x1)
-        x2 = (-b - D) / (2 * a)
-        print("x2 = ", x2)
-    elif D == 0:
-        D = -b / (2 * a)
-        print("Вершина x = ", D)
-    else:
-        print("Корней нет.")
-    return
+def quadratic_equation (a,b,c):
+    import math
+    D = int(b ** 2) - 4 * int(a) * int(c)
+    D = D/(1/2)
+    x1 = (-b + D) / (2 * a)
+    x2 = (-b - D) / (2 * a)
+    return x1,x2
 
 a = int(input("Введите а(первый коэффициент квадр. уравнения): "))
 b = int(input("Введите b(второй коэффициент квадр. уравнения): "))
 c = int(input("Введите c(третий коэффициент квадр. уравнения): "))
-D = int(b ** 2) - 4 * int(a) * int(c)
-print("Дискриминант равен = ", D)
-print(quadratic_equation(a,b,D))
+
+x1,x2 = quadratic_equation(a,b,c)
+print("Корни квадратного уравнения при a=" ,a,",","b=",b ,",","c=",c,",", "равны: ",x1,",",x2)
