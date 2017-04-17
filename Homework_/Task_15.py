777ad9aa0eb658dc74e896c45a8906918a9a5dc4
SancheeZzz <valera.redis@gmail.com>
2017-04-17 15:45:12 +0300

    Task_15 И вот методом проб и ошибок, я вроде бы дошёл до истины)

--- Homework_/Task_15.py
+++ Homework_/Task_15.py
@@ -2,11 +2,12 @@ import math
 
 def intersection_of_circles (x_1,y_1,radius_1,x_2,y_2,radius_2):
     if distance_between_centers > sum_of_radii:
-        print("круги не пересекаются")
+        result = False
     elif distance_between_centers < abs(radius_2-radius_1) :
-        print("Один круг входит в другой")
+        result = False
     else:
-        print("Круги соприкасаются либо пересекаются")
+        result = True
+    return result
 x_1 = int(input("Введите первую координату центра ПЕРВОГО круга: "))
 y_1 = int(input("Введите вторую координату центра ПЕРВОГО круга: "))
 radius_1 = int(input("Введите длину радиуса первого круга: "))
@@ -17,4 +18,7 @@ radius_2 = int(input("Введите длину радиуса второго к
 distance_between_centers = math.sqrt(((x_2-x_1)**2) + ((y_2-y_1)**2))
 sum_of_radii = radius_1+radius_2
 
-print(intersection_of_circles(x_1,y_1,radius_1,x_2,y_2,radius_2))
+if intersection_of_circles(x_1,y_1,radius_1,x_2,y_2,radius_2)== False:
+    print("круги не пересекаются либо один круг входит в другой")
+else:
+    print("Круги соприкасаются либо пересекаются")
