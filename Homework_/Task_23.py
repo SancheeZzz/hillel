ef1e4c77477e72baf82221decb1eeb993ebb474e
SancheeZzz <valera.redis@gmail.com>
2017-05-03 19:23:49 +0300

    Task_23

--- Homework_/Task_23.py
+++ Homework_/Task_23.py
@@ -2,7 +2,6 @@ def factorial(n):
     if n == 0:
         return 1
     return factorial(n - 1) * n
-
 print("---Функция нахождения факториала с числа---")
 n = int(input("Введите число для нахождения факториала: "))
 print("Факториал равен: ", factorial(n))
