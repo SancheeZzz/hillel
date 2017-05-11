f7ea218d66058188266fc771bdd42f775a7dc413
SancheeZzz <valera.redis@gmail.com>
2017-05-11 13:57:24 +0300

    Task_30

--- Homework_/Task_30.py
+++ Homework_/Task_30.py
@@ -1,8 +1,13 @@
 str=input("Введите строку, которую хотите зашифровать")
-tab_of_encryption = ["z",'p',"a","г","i","е","1","2","3","-","5","6"]
+tab_of_encryption = ["a",'b',"c","d","e","f","g","h","i","j","k","l",'m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 str_2 = ""
+
 for i in str:
     idx_1 = str.find(i)
-    elem_of_tab = tab_of_encryption [idx_1+5]
+    idxPlus5 = idx_1 + 5
+    if idxPlus5 > len(tab_of_encryption):
+        elem_of_tab = tab_of_encryption[idxPlus5-len(tab_of_encryption)]
+    else:
+        elem_of_tab = tab_of_encryption [idxPlus5]
     str_2+= elem_of_tab
 print("Зашифрованная строка-",str_2)
