c8fdd35a30a1ca65ed4dd22f0e5da4b2d83f0eb7
SancheeZzz <valera.redis@gmail.com>
2017-05-12 20:21:36 +0300

    Task_4 (TEST)

--- Test/Task_4(TEST).py
+++ Test/Task_4(TEST).py
@@ -4,13 +4,9 @@ while True:
     n = input("введите любое пятизначное число")
     if len(n)<5 or len(n) >5:
         print("Введите 5 символов!")
-        continue
     else:
         for i in n:
-            if int(i)%2==0:
-                continue
-            else:
-                multiply=multiply*int(i)
-                continue
+            if int(i)%2!=0:
+                multiply = multiply * int(i)
         break
 print(multiply)
