5e1ab05d6ce3dce01b2828821701b043d4c0340e
SancheeZzz <valera.redis@gmail.com>
2017-05-15 20:24:26 +0300

    Task_6(TEST)

---
+++ Test/Task_6(TEST).py
@@ -0,0 +1,29 @@
+from math import sqrt
+import random
+#отбираем простые числа в любом диапазоне
+simple_numbers=[]
+
+for i in range (2,101):
+    a = True
+    for n in range (2, int(sqrt(i))+1):
+        if i%n==0:
+            a=False
+            break
+    if a==True:
+        simple_numbers.append(i)
+print(simple_numbers)
+
+#создаём пустой массив
+ten_numbers=[]
+
+#создаем переменную для подсчёта кол-ва цифр в массиве
+count=0
+
+#наполняем массив ранее отобранными простыми числами
+while count<10:
+    rand_num = random.choice(simple_numbers)
+    ten_numbers.append(rand_num)
+    count += 1
+
+#выводим на экран массив из 10 простых чисел, рандомно отобранных
+print(ten_numbers)
