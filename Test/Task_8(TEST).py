19319c28160b3bb58770756967a4d4441ef5a282
SancheeZzz <valera.redis@gmail.com>
2017-05-16 19:17:59 +0300

    Task_8(TEST)

--- Test/Task_8(TEST).py
+++ Test/Task_8(TEST).py
@@ -2,10 +2,11 @@ lst = [1,2,3,4,5,6,7,8,9,10,11,12]
 maximum= max(lst)
 minimum= min(lst)
 for i in lst:
+    idx=i-1
     if i == maximum:
-        lst[i-1]=minimum
+        lst[idx]=minimum
         continue
     if i == minimum:
-        lst[i-1]=maximum
+        lst[idx]=maximum
         continue
 print(lst)
