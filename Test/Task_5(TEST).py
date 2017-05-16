32ede046e260635d1562a612818a5e722caa61fb
SancheeZzz <valera.redis@gmail.com>
2017-05-16 19:17:34 +0300

    Task_5(TEST)

--- Test/Task_5(TEST).py
+++ Test/Task_5(TEST).py
@@ -1,14 +1,14 @@
 print("Программа для вычисления ближайшего к 10 из двух чисел")
 num_1 = int(input ("Введите первое число: "))
 num_2 = int(input ("Введите второе число: "))
-condition_1 = 10 - num_1
-condition_2 = 10 - num_2
+distance_1 = 10 - num_1
+distance_2 = 10 - num_2
 
-if abs(condition_1) < abs(condition_2):
+if abs(distance_1) < abs(distance_2):
     print("Число %d ближе всего к 10" % (num_1))
-elif abs(condition_1) > abs(condition_2):
+elif abs(distance_1) > abs(distance_2):
     print("Число %d ближе всего к 10" % (num_2))
-elif abs(condition_1) == abs(condition_2):
+elif abs(distance_1) == abs(distance_2):
     print("Числа равноотдалённые от 10")
 
 
