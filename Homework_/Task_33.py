1f30fd7fdeef52cf038184bc9fd0a520cef6f1f5
SancheeZzz <valera.redis@gmail.com>
2017-05-28 14:04:25 +0300

    Task_33

---
+++ Homework_/Task_33.py
@@ -0,0 +1,100 @@
+group = [
+    {"id": 1024, "fullname": "Александр Скворцов",    "email": "", "github": "https://github.com/Agskvortsov", "rank": 0},
+    {"id": 1025, "fullname": "Андрей Рожко",          "email": "", "github": "https://github.com/rozhkoandrew", "rank": 0},
+    {"id": 1026, "fullname": "Кулишенко Алексей",     "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1027, "fullname": "Виталий Рыжков",        "email": "", "github": "https://github.com/vitalyryzhkov", "rank": 0},
+    {"id": 1028, "fullname": "Гавеля Виталина",       "email": "", "github": "https://github.com/", "rank": 0},
+    {"id": 1029, "fullname": "Виктор Бурлаков",       "email": "", "github": "https://github.com/SancheeZzz", "rank": 0},
+    {"id": 1030, "fullname": "Виктор Горовой",        "email": "", "github": "https://github.com/lamboosanproject", "rank": 0},
+    {"id": 1031, "fullname": "Надежда Симанович",     "email": "", "github": "https://github.com/simanadya", "rank": 0},
+    {"id": 1032, "fullname": "Николай Марушевский",   "email": "", "github": "https://github.com/Gelios-Sky", "rank": 0},
+    {"id": 1033, "fullname": "Андрей Кравчук",        "email": "", "github": "https://github.com/albaderon", "rank": 0},
+    {"id": 1034, "fullname": "Шадрина Екатерина",     "email": "", "github": "https://github.com/katefeline", "rank": 0},
+    {"id": 1035, "fullname": "Александр Малышев",     "email": "", "github": "https://github.com/Barabasha", "rank": 0},
+    {"id": 1036, "fullname": "Владимир Веренчук",     "email": "", "github": "https://github.com/", "rank": 0}
+]
+
+hw_results = [
+    {"id": 1024, "task_completion": [1,1,1,1,1,1,1,0,1,0,1,0,0,1,0,1,1,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1034, "task_completion": [1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0] },
+    {"id": 1036, "task_completion": [1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+
+test1_results = [
+    {"id": 1024, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1026, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1028, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,0,0,0,0,0,0,0,0] },
+    {"id": 1032, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,1,1,1,0,0,1] },
+    {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,1,1,1,0,1,1] },
+    {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+test1_weights = {
+    1 : 1,
+    2 : 1,
+    3 : 1,
+    4 : 2,
+    5 : 2,
+    6 : 2,
+    7 : 4,
+    8 : 4,
+    9 : 4,
+    10 : 8,
+    11 : 8,
+    12 : 15
+}
+
+
+def update_students_results(group):
+    for entry in group:
+        id = entry["id"]
+        rank=0
+        tasks_completed =0
+        for t_result in test1_results:
+            if t_result['id']==id:
+                tasks_complete=t_result['task_completion']
+                for idx,weight in enumerate(test1_weights):
+                    calculate=tasks_complete[idx-1]*test1_weights.get(weight)
+                    rank+=calculate
+        for hw_result in hw_results:
+            if hw_result['id']==id:
+                homework=hw_result['task_completion']
+                for elem in homework:
+                    if elem == 1:
+                        tasks_completed+=1
+        entry['rank']=rank+tasks_completed
+
+
+def print_students_info(group, sort_by_key="fullname"):
+    for person in group:
+        print(": ID:         %10s :" % person['id'])
+        print(":.................:")
+        print(": Full name:  %10s :" % person['fullname'])
+        print(": Email:      %10s :" % person['email'])
+        print(": Github:     %20s :" % person['github'])
+        print(": Rank:       %10s :" % person['rank'])
+        print("-----------------")
+
+
+
+if __name__ == "__main__":
+    update_students_results(group)
+print_students_info(group, "rank")
